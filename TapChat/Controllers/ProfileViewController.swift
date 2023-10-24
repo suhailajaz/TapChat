0e3a2806de657a36efc957f7c5f01779f266122f
suhail <suhailajaz338@gmail.com>
2023-10-24 18:40:48 +0530

    Directory structure setup and Initial Controllers added

---
+++ TapChat/Controllers/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  TapChat
+//
+//  Created by suhail on 24/10/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
