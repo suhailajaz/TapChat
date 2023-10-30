0284dcc1e7566c9c6bcb24596925f41ecbe4f2af
suhail <suhailajaz338@gmail.com>
2023-10-30 23:37:04 +0530

    Configured registraion and added authenticated user credentials to realtime database

---
+++ TapChat/Resources/AuthManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthManager.swift
+//  TapChat
+//
+//  Created by suhail on 25/10/23.
+//
+
+import Foundation
---
+++ TapChat/Resources/DatabaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseManager.swift
+//  TapChat
+//
+//  Created by suhail on 30/10/23.
+//
+
+import Foundation
